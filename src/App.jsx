6c325b18ea31bf706f34050fa9aef4e305aa871e
Eric Xie <ericxie6@gmail.com>
2023-04-08 11:19:31 -0400

    timer screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import './App.css'
-import "bulma/css/bulma.css"
+import "./sass/mystyles.scss"
 import Timer from "./components/Timer"
 
 
@@ -9,42 +8,50 @@ function App() {
 
   let content = <h1>OOPS</h1>
 
-  switch(tab) {
-    case "timer": 
+  switch (tab) {
+    case "timer":
       content = <Timer />
       break;
   }
 
   return (
-    <div className = "App">
-      <div className="body">
+    <section className='hero is-fullheight'>
+      <div className="hero-body">
         {content}
       </div>
 
-      <div className="tabs is-centered is-boxed is-medium">
-        <ul>
-          <li className={tab == "timer" ? "is-active" : ""}>
-            <a onClick={() => setTab("timer")}>
-              <span className="icon is-small"><i className="fas fa-image" aria-hidden="true"></i></span>
-              <span>Pictures</span>
-            </a>
-          </li>
-          <li className={tab == "user" ? "is-active" : ""}>
-            <a onClick={() => setTab("user")}>
-              <span className="icon is-small"><i className="fas fa-music" aria-hidden="true"></i></span>
-              <span>Music</span>
-            </a>
-          </li>
-          <li className={tab == "settings" ? "is-active" : ""}>
-            <a onClick={() => setTab("settings")}>
-              <span className="icon is-small"><i className="fas fa-film" aria-hidden="true"></i></span>
-              <span>Videos</span>
-            </a>
-          </li>
-        </ul>
+      <div className="hero-foot">
+        <div className="tabs is-centered is-boxed is-medium is-fullwidth">
+          <ul>
+            <li className={tab == "timer" ? "is-active" : ""}>
+              <a onClick={() => setTab("timer")}>
+                <span className="icon is-small"><i className="fas fa-image" aria-hidden="true"></i></span>
+                <span>Timer</span>
+              </a>
+            </li>
+            <li className={tab == "user" ? "is-active" : ""}>
+              <a onClick={() => setTab("user")}>
+                <span className="icon is-small"><i className="fas fa-music" aria-hidden="true"></i></span>
+                <span>User</span>
+              </a>
+            </li>
+            <li className={tab == "settings" ? "is-active" : ""}>
+              <a onClick={() => setTab("settings")}>
+                <span className="icon is-small"><i className="fas fa-film" aria-hidden="true"></i></span>
+                <span>Settings</span>
+              </a>
+            </li>
+            <li className={tab == "about" ? "is-active" : ""}>
+              <a onClick={() => setTab("about")}>
+                <span className="icon is-small"><i className="fas fa-film" aria-hidden="true"></i></span>
+                <span>About</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
-export default App
+export default App;
